9115639bdf8fa9bbc6e0a3c54c0bbe3d8f3ca3d5
jKrovn <krovn@pm.me>
2022-10-31 13:46:43 -0400

    chapter 6

--- Ecom-Back/cmd/api/movie-handlers.go
+++ Ecom-Back/cmd/api/movie-handlers.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"backend/models"
 	"errors"
-	"log"
+	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -16,26 +14,66 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil {
 		app.logger.Println(errors.New("invalid id parameter"))
+		app.errorJSON(w, err)
+		return
 	}
-	app.logger.Println("id is", id)
-	movie := models.Movie{
-		ID:          id,
-		Title:       "Some movie",
-		Description: "Some description",
-		Year:        2021,
-		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-		Runtime:     100,
-		Rating:      5,
-		MPAARating:  "PG-13",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
+	movie, err := app.models.DB.Get(id)
+	if err != nil {
+		fmt.Println(err)
 	}
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
 	if err != nil {
-		log.Fatalln(err)
+		app.errorJSON(w, err)
 	}
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+}
+
+func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
+	genres, err := app.models.DB.GenresAll()
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+	err = app.writeJSON(w, http.StatusOK, genres, "genres")
+}
+
+func (app *application) getAllMoviesByGenre(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	genreID, err := strconv.Atoi(params.ByName("genre_id"))
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+	movies, err := app.models.DB.All(genreID)
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
 
 }
--- Ecom-Front/src/App.js
+++ Ecom-Front/src/App.js
@@ -3,8 +3,10 @@ import {BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch} 
 import Movies from './components/Movies';
 import Admin from './components/Admin';
 import Home from './components/Home';
-import Categories from './components/Categories';
 import OneMovie from './components/OneMovie';
+import Genres from './components/Genres';
+import OneGenre from './components/OneGenre';
+
 
 export default function App() {
   return (
@@ -29,7 +31,7 @@ export default function App() {
                 <Link to="/movies">Movies</Link>
               </li>
               <li className="list-group-item">
-                <Link to="/by-category">Categories</Link>
+                <Link to="/genres">Genres</Link>
               </li>
               <li className="list-group-item">
                 <Link to="/admin">Manage Catalogue</Link>
@@ -40,28 +42,15 @@ export default function App() {
 
         <div className="col-md-10">
           <Switch>
-            
-            <Route path="/movies/:id" component={OneMovie} />
-
+          <Route path="/movies/:id" component={OneMovie} />
             <Route path="/movies">
               <Movies />
             </Route>
-            <Route exact path="/by-category">
-              <CategoryPage />
+            <Route path="/genre/:id" component={OneGenre} />
+
+            <Route exact path="/genres">
+              <Genres />
             </Route>
-
-            <Route 
-            exact
-            path="/by-category/drama"
-            render={(props) => <Categories {...props} title={`Drama`} />}
-            />
-
-          <Route 
-            exact
-            path="/by-category/comedy"
-            render={(props) => <Categories {...props} title={`Comedy`} />}
-            />
-
             <Route path="/admin">
               <Admin />
             </Route>
@@ -76,18 +65,3 @@ export default function App() {
   );
 }
 
-function CategoryPage() {
-
-  let { path, url } = useRouteMatch();
-
-  return (
-    <div>
-      <h2>Categories</h2>
-
-      <ul>
-        <li><Link to={`${path}/comedy`}>Comedy</Link> </li>
-        <li><Link to={`${url}/drama`}>Drama</Link> </li>
-      </ul>
-    </div>
-  );
-}
