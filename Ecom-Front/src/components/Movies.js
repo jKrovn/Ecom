65a3e0add9ec269e93e9a8d9929869cd64489560
jKrovn <krovn@pm.me>
2022-11-07 10:47:16 -0500

    finish chapter 11

--- Ecom-Front/src/components/Movies.js
+++ Ecom-Front/src/components/Movies.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 export default class Movies extends Component {
@@ -6,34 +6,37 @@ export default class Movies extends Component {
     movies: [],
     isLoaded: false,
     error: null,
-  };
+  }
 
   componentDidMount() {
-    fetch("http://localhost:4000/v1/movies")
-      .then(response => {
-        console.log("Status code is", response.status)
-        if (response.status !== "200") {
-          let err = Error;
-          err.message = "Invalid response code " + response.status;
+    fetch('http://localhost:4000/v1/movies')
+      .then((response) => {
+        console.log('Status code is', response.status)
+        if (response.status !== 200) {
+          let err = Error
+          err.message = 'Invalid response code ' + response.status
           this.setState({ error: err })
         }
         return response.json()
       })
-      .then(json => {
-        this.setState({
-          movies: json.movies,
-          isLoaded: true,
-        }, (error) => {
-          this.setState({
+      .then((json) => {
+        this.setState(
+          {
+            movies: json.movies,
             isLoaded: true,
-            error
-          })
-        })
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            })
+          }
+        )
       })
   }
 
   render() {
-    const { movies, isLoaded, error } = this.state;
+    const { movies, isLoaded, error } = this.state
     if (error) {
       return <div>Error: {error.message}</div>
     } else if (!isLoaded) {
@@ -44,11 +47,17 @@ export default class Movies extends Component {
           <h2>Choose a movie</h2>
           <div className='list-group'>
             {movies.map((m) => (
-              <Link key={m.id} className='list-group-item list-group-item-action' to={`/movies/${m.id}`}>{m.title}</Link>
+              <Link
+                key={m.id}
+                className='list-group-item list-group-item-action'
+                to={`/movies/${m.id}`}
+              >
+                {m.title}
+              </Link>
             ))}
           </div>
         </Fragment>
-      );
+      )
     }
   }
 }
